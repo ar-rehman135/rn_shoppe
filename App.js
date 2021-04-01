80abed6ee580bfc2206a5de257967ff72a8d253e
ar-rehman135 <abdulrehmanmirza135@gmail.com>
2021-04-01 13:27:43 +0500

    Shoppe Project

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import { enableScreens } from 'react-native-screens';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import store from './store/store';
+import NavigationContainer from './navigation/NavigationContainer';
+
+enableScreens();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
+const App = () => {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  return fontLoaded ? (
+    <Provider store={store}>
+      <NavigationContainer />
+    </Provider>
+  ) : (
+    <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => {
+        setFontLoaded(true);
+      }}
+      onError={(error)=>{
+        console.log("error");
+        console.log(error);
+      }}
+    />
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Trading App React Native
+
+An eCommerce app built with Expo CLI stage in React Native. Firebase is utilized as backend. Libraries utilized are Respond Route, Redux, Redux Thunk, Respond Local Revived, Respond Local Motion Handler, etc.
